171c2131687656441c0b41b1d780e1d2cbf20700
Justin Fyfe <justin@fyfesoftware.ca>
2023-01-06 14:41:24 -0500

    Updated the assembly information and debugger architecture

---
+++ Solution Items/cmdprompt.cmd
@@ -0,0 +1,17 @@
+@echo off
+set path=%path%;%cd%
+cd "%1"
+
+echo SanteDB Software Development Kit 3.0
+echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+echo.
+echo There are several tools which are useful for debugging SanteDB:
+echo.
+echo	sdb-bb		-	A tool for extracting files from connected Android devices (requires ADB on path)
+echo	adb-ade		-	A tool which allows you to debug your applets in real time in an edit/save/refresh cycle
+echo	sdb-brd		-	Debugging tool for business rules and clinical protocols
+echo	sdb-vmu		-	View Model Utility for generating JavaScript or C# serializers
+echo	sdb-vocab		-	A tool for importing vocabulary into Datasets
+echo	pakman		-	A tool for packaging your applet files for distribution
+echo	sdbac			-	SanteDB Administrative Console	
+echo Successfully added to path..
