4bcf13ebb4f63ad3059437a68847fbb0b1011169
Justin Fyfe <justin@fyfesoftware.ca>
2023-05-12 13:48:04 -0400

    Added additional resources for sync and fixed some of the tagging to use constants

--- PakSrv/Properties/Version.cs
+++ PakSrv/Properties/Version.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("3.0.800")]
 [assembly: System.Reflection.AssemblyFileVersion("3.0.800")]
+[assembly: System.Reflection.AssemblyVersion("3.0.800")]
 
 
