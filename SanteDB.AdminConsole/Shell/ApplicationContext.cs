e43a62ca9d6ad6d124406c0332700b7289f78ffd
Justin Fyfe <justin@fyfesoftware.ca>
2025-07-11 12:02:38 -0400

    Updated the admin shell

--- SanteDB.AdminConsole/Shell/ApplicationContext.cs
+++ SanteDB.AdminConsole/Shell/ApplicationContext.cs
@@ -25,6 +25,7 @@ using SanteDB.Core.Diagnostics;
 using SanteDB.Core.Http;
 using SanteDB.Core.Http.Description;
 using SanteDB.Core.Interop;
+using SanteDB.Core.Services;
 using SanteDB.Messaging.AMI.Client;
 using System;
 using System.Collections.Generic;
@@ -158,7 +159,7 @@ namespace SanteDB.AdminConsole.Shell
             m_services.Add(new OAuthBearerCredentialProvider());
             m_services.Add(new HttpBasicCredentialProvider());
             m_services.Add(new ConsoleCertificateValidator());
-            m_services.Add(new Client.OAuth.OAuthClientCore(GetService<IRestClientFactory>()) { ClientId = Configuration.AppId });
+            m_services.Add(new Client.OAuth.OAuthClientCore(GetService<IRestClientFactory>(), GetService<IAdhocCacheService>()) { ClientId = Configuration.AppId });
         }
 
         /// <summary>
